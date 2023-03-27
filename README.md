aff7f1d06a6d0a387cd329f075450c939d806d88
Scott Gregory <dsg.gregory@gmail.com>
2023-03-27 11:51:31 -0400

    doc

--- README.md
+++ README.md
@@ -1,6 +1,37 @@
 # WebAuthN Example in Go
 
+This is a Demo to provide insight into a WebAuthn flow.
+
+## Caveats
+
+### HTTP(s) Only
+This demo does not use TLS so only a connection to http://localhost:8080 will be able to perform WebAuthn functions. I've only tested with Safari on a Mac.
+
+## Demo Walk Through
+### Registration
+The [javascript](./views/index.js) communicates with the Safari browser to provide a series of dialogs that collect information about which security device will be used to authenticate this user in the future.  
+
+![After clicking register button](./doc/SafariRegisterDialog.png)
+
+After providing an email address and clicking the **Register** button, the `/register/begin/{username}` endpoint is called to create a user record and generate a public key for the user. Safari then presents this dialog in response to the javascript function `navigator.credentials.create()`. You may choose to use the token provided by the OS (your laptop login token) when clicking the **Continue** button, or for this demo, choosing to use a YubiKey by clicking **Other Options**.
+
+![After clicking "Other Options" from "save passkey" dialog](./doc/SafariRegisterSecurityKey.png)
+
+This dialog prompts you for the type of token device to be used. On Mac Safari you may have options including the OS login token, using your paired iPhone as token device, or for this example, choosing the **Security Key** to use a YubiKey.
+
+![Choose to use YubiKey](./doc/SafariRegisterEnterSecurityKey.png)
+
+After pressing **Continue** from the previous dialog, Safari presents this dialog and waits for you to press/activate your YubiKey. Safari reads the YubiKey OTP token and validates it with YubiCo. The token is then passed back to the javascript provided in the [index.html](./views/index.js) which calls the `/register/finish/{username}` endpoint to store the user credentials to validate a device token at login. 
+
+### Login
+Using the [javascript](./views/index.js) which calls the `/login/begin/{username}` and `/login/finish/{username}` endpoints, the Safari browser collects the authentication token from the system or requests it from the user.
+
+Login begin sets the `webauthn-session` cookie and returns the public key for the user found in the DB. The javascript in the browser gathers the assertion made by the browser itself. Then calls the finish endpoint to validate the token data sent by the browser.
+
+From here on, server endpoints can simply validate the `webauthn-session` cookie. Once the session expires, /login should be called again by the javascript. To logout, the session cookie should be destroyed.
+
 ## Resources
 * https://www.herbie.dev/blog/webauthn-basic-web-client-server/
 * https://github.com/go-webauthn/webauthn/blob/master/README.md#quickstart
 * https://fidoalliance.org/specs/fido-v2.0-rd-20180702/fido-server-v2.0-rd-20180702.html#registration
+* https://github.com/Quiq/webauthn_proxy
---
+++ pkg/api/server.go
@@ -0,0 +1,250 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/go-webauthn/webauthn/protocol"
+	"github.com/go-webauthn/webauthn/webauthn"
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+	"net/http"
+	"strings"
+	"webauthn/pkg/database"
+	"webauthn/pkg/session"
+)
+
+type Server struct {
+	listenAddr string
+	mux        *mux.Router
+
+	webAuthn     *webauthn.WebAuthn
+	sessionStore *session.Store
+	userDB       *database.UserDB
+}
+
+func jsonResponse(w http.ResponseWriter, d interface{}, c int) {
+	dj, err := json.Marshal(d)
+	if err != nil {
+		http.Error(w, "Error creating JSON response", http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(c)
+	fmt.Fprintf(w, "%s", dj)
+
+	logrus.WithField("httpStatusCode", c).Debug(string(dj))
+}
+
+// the browser javascript calls this after the user presses the `Register` button
+func (s *Server) beginRegistration(w http.ResponseWriter, r *http.Request) {
+
+	// get username
+	vars := mux.Vars(r)
+	username, ok := vars["username"]
+	if !ok {
+		jsonResponse(w, fmt.Errorf("must supply a valid username i.e. foo@bar.com"), http.StatusBadRequest)
+		return
+	}
+
+	l := logrus.WithField("username", username)
+	l.Debug("BeginRegistration")
+
+	// get user
+	user, err := s.userDB.GetUser(username)
+	// user doesn't exist, create new user
+	if err != nil {
+		displayName := strings.Split(username, "@")[0]
+		user = database.NewUser(username, displayName)
+		s.userDB.PutUser(user)
+	}
+
+	// generate PublicKeyCredentialCreationOptions, session data
+	registerOptions := func(credCreationOpts *protocol.PublicKeyCredentialCreationOptions) {
+		credCreationOpts.CredentialExcludeList = user.CredentialExcludeList()
+	}
+
+	options, sessionData, err := s.webAuthn.BeginRegistration(
+		user,
+		registerOptions,
+	)
+
+	if err != nil {
+		l.WithError(err).Error("BeginRegistration failed")
+		jsonResponse(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// store session data as marshaled JSON
+	err = s.sessionStore.SaveWebauthnSession("registration", sessionData, r, w)
+	if err != nil {
+		l.WithError(err).Error("SaveWebauthnSession")
+		jsonResponse(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	jsonResponse(w, options, http.StatusOK)
+}
+
+// The browser javascript calls this after browser asks user for creds or browser collects the private creds data from browser's store
+func (s *Server) finishRegistration(w http.ResponseWriter, r *http.Request) {
+
+	// get username
+	vars := mux.Vars(r)
+	username := vars["username"]
+
+	l := logrus.WithField("username", username)
+	l.Debug("Finishregistration")
+
+	// get user
+	user, err := s.userDB.GetUser(username)
+	// user doesn't exist
+	if err != nil {
+		l.WithError(err).Error("user not found")
+		jsonResponse(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	// load the session data
+	sessionData, err := s.sessionStore.GetWebauthnSession("registration", r)
+	if err != nil {
+		l.WithError(err).Error("GetWebauthnSession failed")
+		jsonResponse(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	credential, err := s.webAuthn.FinishRegistration(user, sessionData, r)
+	if err != nil {
+		l.WithError(err).Error("FinishRegistration failed")
+		jsonResponse(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	user.AddCredential(*credential)
+
+	jsonResponse(w, "Registration Success", http.StatusOK)
+	return
+}
+
+func (s *Server) beginLogin(w http.ResponseWriter, r *http.Request) {
+
+	// get username
+	vars := mux.Vars(r)
+	username := vars["username"]
+
+	l := logrus.WithField("username", username)
+	l.Debug("BeginLogin")
+
+	// get user
+	user, err := s.userDB.GetUser(username)
+
+	// user doesn't exist
+	if err != nil {
+		l.WithError(err).Error("no such user")
+		jsonResponse(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	// generate PublicKeyCredentialRequestOptions, session data
+	options, sessionData, err := s.webAuthn.BeginLogin(user)
+	if err != nil {
+		l.WithError(err).Error("BeginLogin failed")
+		jsonResponse(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// store session data as marshaled JSON
+	err = s.sessionStore.SaveWebauthnSession("authentication", sessionData, r, w)
+	if err != nil {
+		l.WithError(err).Error("SaveWebauthnSession failed")
+		jsonResponse(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	jsonResponse(w, options, http.StatusOK)
+}
+
+func (s *Server) finishLogin(w http.ResponseWriter, r *http.Request) {
+
+	// get username
+	vars := mux.Vars(r)
+	username := vars["username"]
+
+	l := logrus.WithField("username", username)
+	l.Debug("FinishLogin")
+
+	// get user
+	user, err := s.userDB.GetUser(username)
+
+	// user doesn't exist
+	if err != nil {
+		l.WithError(err).Error("no such user")
+		jsonResponse(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	// load the session data
+	sessionData, err := s.sessionStore.GetWebauthnSession("authentication", r)
+	if err != nil {
+		l.WithError(err).Error("GetWebauthnSession failed")
+		jsonResponse(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// in an actual implementation we should perform additional
+	// checks on the returned 'credential'
+	_, err = s.webAuthn.FinishLogin(user, sessionData, r)
+	if err != nil {
+		l.WithError(err).Error("FinishLogin failed")
+		jsonResponse(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// handle successful login
+	jsonResponse(w, "Login Success", http.StatusOK)
+}
+
+// Start
+func (s *Server) Start() {
+	logrus.Info("starting server at", s.listenAddr)
+	logrus.Fatal(http.ListenAndServe(s.listenAddr, s.mux))
+}
+
+// NewServer
+func NewServer(addr string) (*Server, error) {
+	a := strings.Split(addr, ":")
+	if len(a) != 2 {
+		return nil, fmt.Errorf("requires hostname from listen address to provide as RPOrigin")
+	}
+
+	s := &Server{listenAddr: addr}
+
+	var err error
+	s.webAuthn, err = webauthn.New(&webauthn.Config{
+		RPDisplayName:        "Foobar Corp.",                     // display name for your site
+		RPID:                 "localhost",                        // generally the domain name for your site
+		RPOrigins:            []string{"http://" + s.listenAddr}, // The origin URLs allowed for WebAuthn requests
+		EncodeUserIDAsString: false,                              // is/not URLEncodedBase64
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("%w; failed to create WebAuthn from config", err)
+	}
+
+	s.userDB = database.DB()
+
+	s.sessionStore, err = session.NewStore()
+	if err != nil {
+		return nil, fmt.Errorf("%w; failed to create session store", err)
+	}
+
+	r := mux.NewRouter()
+	s.mux = r
+
+	r.HandleFunc("/register/begin/{username}", s.beginRegistration).Methods("GET")
+	r.HandleFunc("/register/finish/{username}", s.finishRegistration).Methods("POST")
+	r.HandleFunc("/login/begin/{username}", s.beginLogin).Methods("GET")
+	r.HandleFunc("/login/finish/{username}", s.finishLogin).Methods("POST")
+
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./views")))
+
+	return s, nil
+}
